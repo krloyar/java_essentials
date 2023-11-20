f98d769ca8ea4f37fe08b3967cc4f2f0610a82de
krloyar <krloyar@gmail.com>
2023-11-20 06:56:29 +0000

    day2-second commit

---
+++ Grettings.java
@@ -0,0 +1,5 @@
+public class Grettings {
+    public static void main(String[] args) {
+        System.out.println("Dudho nahao, puto phalo ");
+    }
+}
--- Swap.java
+++ Swap.java
@@ -7,9 +7,9 @@ public class Swap {
         System.out.println("Values before swap: ");
         System.out.println("n1: " +n1 + " n2: "+n2);
 
-        int vada = n1;
-        n1 = n2;
-        n2 = vada;
+        n1 = n1 + n2; // 10 + 20
+        n2 = n1 - n2; // 30 - 20 = 10
+        n1 = n1 - n2; // 30 - 10 = 20
 
         System.out.println("Values after swap: ");
         System.out.println("n1: " +n1 + " n2: "+n2);
